512d22643497700a9127fbdbee7d3ee723c7e7e1
hoshi0405 <guracocoro.3434@gmail.com>
2023-02-13 06:32:18 +0900

    BASEURL

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "http://localhost:3000/api/v1" || "https://nodejs-ramenjiro-sercher.herokuapp.com/api/v1";
 const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
