5bb0335bae0b8d4292a8daf4a65b9ea6d5105bc5
hoshi0405 <guracocoro.3434@gmail.com>
2023-06-02 06:32:58 +0900

    ボタン形式変更

--- src/components/Atoms/StoreSreach.jsx
+++ src/components/Atoms/StoreSreach.jsx
@@ -1,7 +1,7 @@
 const PlaceSearchButton = (props) => {
   return (
-    <button type="button" style="color:#ff0000" onClick={props.onClick}>
-      近くのラーメン二郎
+    <button type="button" onClick={props.onClick}>
+      近くのラーメン
     </button>
   );
 };
